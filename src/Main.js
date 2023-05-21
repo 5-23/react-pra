07b47f3f3856814764b8aaca318462ed3905c8aa
5-23 dev <86705803+5-23@users.noreply.github.com>
2023-05-22 07:47:14 +0900

    init

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './Main.css';
+
+function Comp(){
+	
+}
+
+function Main() {
+	return (
+		<main>
+			
+			<h1>hi</h1>
+
+		</main>
+	);
+}
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Main from './Main';
+
+const root = ReactDOM.createRoot(document.body);
+root.render(
+	<React.StrictMode>
+    	<Main />
+  	</React.StrictMode>
+);
