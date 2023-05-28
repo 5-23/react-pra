6e6413fcb32e99bb5d6517c2a40c8e2ba6fd383c
5-23 <yhanbyeol6@gmail.com>
2023-05-28 20:33:39 +0900

    .

--- src/Main.tsx
+++ src/Main.tsx
@@ -21,11 +21,15 @@ export default function Main(){
 					post_data[1](value)
 					change_modal(false)
 				}}>
-					<input type="text" name="title" id="title" placeholder="title" ref={title}/> <br/>
-					<input type="text" name="des" id="des" placeholder="description" ref={des}/> <br/>
+					<h3>title</h3>
+					<input type="text" name="title" id="title" placeholder="title" ref={title} required/> <br/>
+					
+					<h3>description</h3>
+					<input type="text" name="des" id="des" placeholder="description" ref={des} required/> <br/>
 					<input type="submit" value="submit" />
 				</form>
 			</div>
+
 			<input id="modal-sw" type="button" value="+" onClick={() => change_modal(!modal)}/>
 			<Post arr={post_data[0]}></Post>
 		</main>
